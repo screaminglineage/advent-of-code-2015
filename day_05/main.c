af5515017942e8d108bb1039c319e1a50807f819
Aditya Ghosh <adityaghosh5025@gmail.com>
2024-11-21 12:52:05 +0530

    finished part_2

--- day_05/main.c
+++ day_05/main.c
@@ -62,13 +62,25 @@ bool letters_match_2(char *string) {
             return true;
         }
     }
+}
+
+bool pair_appears_twice(char *string) {
+    for (size_t i = 0; string[i] && string[i+1]; i++) {
+        // search for identical pair to string[i], string[i+1]
+        for (size_t j = i+2; string[j] && string[j+1]; j++) {
+            if (string[i] == string[j] && string[i+1] == string[j+1]) {
+                return true;
+            }
+        }
+    }
     return false;
 }
+
 int part2(char *data) {
     char *line = strtok(data, "\n");
     int nice_strings = 0;
     while (line) {
-        if (letters_match_2(line)) {
+        if (letters_match_2(line) && pair_appears_twice(line)) {
             nice_strings += 1;
         }
 	    line = strtok(NULL, "\n");
@@ -82,7 +94,7 @@ int main(int argc, char** argv) {
     char* input_file = argv[1];
     char* data = read_to_str(input_file);
 
-    printf("Part 1: %d\n", part1(data));
+    // printf("Part 1: %d\n", part1(data));
     printf("Part 2: %d\n", part2(data));
     return 0;
 }
