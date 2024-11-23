1d22a6ae6e6baa5c07c514c02ad370cf1a3b1e2e
Aditya Ghosh <adityaghosh5025@gmail.com>
2024-11-23 14:38:37 +0530

    failed attempt at part 2 without a json parser

--- day_12/main.c
+++ day_12/main.c
@@ -13,7 +13,7 @@ int part1(char *data) {
         if (*data == '-' && isdigit(data[1])) {
             data++;
         }
-        while (*data && isdigit(*data)) data++;
+        while (isdigit(*data)) data++;
         strncpy(buf, start, data - start);
         count += atoi(buf);
         data += 1;
@@ -21,9 +21,74 @@ int part1(char *data) {
     return count;
 }
 
-int part2(char *data) {
-    return 0;
+bool starts_with(char *string, char *prefix) {
+    if (!prefix[0]) return !string[0];
+    size_t i = 0;
+    for (; string[i] && prefix[i]; i++) {
+        if (string[i] != prefix[i]) {
+            return false;
+        }
+    }
+    return !prefix[i];
 }
+
+typedef struct {
+    char *data;
+    bool within_array;
+    int level;
+    int temp_count;
+    int count;
+} State;
+
+bool inc_state(State *state) {
+    if (state->data[0] == '\0') return false;
+
+    if (state->data[0] == '[') {
+        state->within_array = true;
+    } else if (state->data[0] == ']') {
+        state->within_array = false;
+    } else if (state->data[0] == '{') {
+        state->within_array = false;
+        state->level += 1;
+    } else if (state->data[0] == '}') {
+        state->level -= 1;
+        state->count += state->temp_count;
+        state->temp_count = 0;
+    }
+    state->data++;
+    return true;
+}
+
+int parse_num(State *state) {
+    char buf[16] = {0};
+    char *start = state->data;
+    if (state->data[0] == '-' && isdigit(state->data[1])) {
+        state->data++;
+    }
+    while (isdigit(state->data[0])) state->data++;
+    strncpy(buf, start, state->data - start);
+    return atoi(buf);
+}
+
+int part2(char *data) {
+    State state = {0};
+    state.data = data;
+    while (inc_state(&state)) {
+        if (starts_with(state.data, "\"red\"") && !state.within_array) {
+            state.temp_count = 0;
+            int current_level = state.level;
+            while (inc_state(&state)) {
+                if (state.level < current_level) {
+                    break;
+                }
+            }
+        }
+        state.temp_count += parse_num(&state);
+    }
+    state.count += state.temp_count;
+    return state.count;
+}
+
 
 int main(int argc, char** argv) {
     (void)argc;
