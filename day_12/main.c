7eeb30c6a0859703b1e4714f9ddb98d882df6123
Aditya Ghosh <adityaghosh5025@gmail.com>
2024-11-25 13:19:44 +0530

    finished day 12 part 2 without json parser

--- day_12/main.c
+++ day_12/main.c
@@ -33,60 +33,82 @@ bool starts_with(char *string, char *prefix) {
 }
 
 typedef struct {
-    char *data;
-    bool within_array;
-    int level;
-    int temp_count;
     int count;
+    bool has_red;
+} Count;
+
+
+typedef struct {
+    Count *data;
+    size_t size;
+    size_t capacity;
+} Stack;
+
+
+typedef struct {
+    char *data;
+    Stack count_stack;
 } State;
 
 bool inc_state(State *state) {
     if (state->data[0] == '\0') return false;
 
-    if (state->data[0] == '[') {
-        state->within_array = true;
-    } else if (state->data[0] == ']') {
-        state->within_array = false;
-    } else if (state->data[0] == '{') {
-        state->within_array = false;
-        state->level += 1;
+    size_t top = state->count_stack.size - 1;
+
+    if (state->data[0] == '{') {
+        DYN_APPEND(&(state->count_stack), ((Count){0, false}));
     } else if (state->data[0] == '}') {
-        state->level -= 1;
-        state->count += state->temp_count;
-        state->temp_count = 0;
+        if (!state->count_stack.data[top].has_red) {
+            state->count_stack.data[top - 1].count += state->count_stack.data[top].count;
+        }
+        state->count_stack.size -= 1;
+    } else if (state->data[0] == '[') {
+        DYN_APPEND(&(state->count_stack), ((Count){0, false}));
+    } else if (state->data[0] == ']') {
+        state->count_stack.data[top - 1].count += state->count_stack.data[top].count;
+        state->count_stack.size -= 1;
+    } else if (starts_with(state->data, "\"red\"")) {
+        state->count_stack.data[top].has_red = true;
     }
     state->data++;
     return true;
 }
 
-int parse_num(State *state) {
+bool parse_num(State *state, int *num) {
+    if (!isdigit(state->data[0]) && !(state->data[0] == '-' && isdigit(state->data[1]))) {
+        return false;
+    }
+
     char buf[16] = {0};
     char *start = state->data;
-    if (state->data[0] == '-' && isdigit(state->data[1])) {
-        state->data++;
-    }
+    if (state->data[0] == '-') state->data++;
     while (isdigit(state->data[0])) state->data++;
+
     strncpy(buf, start, state->data - start);
-    return atoi(buf);
+    *num = atoi(buf);
+    return true;
+}
+
+int sum(Stack count_stack) {
+    int sum = 0;
+    for (size_t i = 0; i < count_stack.size; i++) {
+        printf("count_stack.data[i].count = %d\n", count_stack.data[i].count);
+        sum += count_stack.data[i].count;
+    }
+    return sum;
 }
 
 int part2(char *data) {
     State state = {0};
     state.data = data;
+    DYN_APPEND(&(state.count_stack), ((Count){0, false}));
     while (inc_state(&state)) {
-        if (starts_with(state.data, "\"red\"") && !state.within_array) {
-            state.temp_count = 0;
-            int current_level = state.level;
-            while (inc_state(&state)) {
-                if (state.level < current_level) {
-                    break;
-                }
-            }
+        int num = 0;
+        if (parse_num(&state, &num)) {
+            state.count_stack.data[state.count_stack.size - 1].count += num;
         }
-        state.temp_count += parse_num(&state);
     }
-    state.count += state.temp_count;
-    return state.count;
+    return state.count_stack.data[0].count;
 }
 
 
