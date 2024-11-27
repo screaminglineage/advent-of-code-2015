ca04b6638f44e64f6bfc5d7013f2976173dcba06
Aditya Ghosh <adityaghosh5025@gmail.com>
2024-11-27 12:30:02 +0530

    finished part 1

--- day_14/main.c
+++ day_14/main.c
@@ -4,13 +4,16 @@
 #include <string.h>
 #include "../aoc.h"
 
+#define MAX_TIME 2503
+#define REINDEERS 9
+
 int distance(int speed, int time_fly, int time_rest) {
-    int time_left = 1000;
+    int time_left = MAX_TIME;
     int time_unit = time_fly + time_rest;
 
     int distance = 0;
     while (time_left > 0) {
-        printf("time_left = %d\n", time_left);
+        // printf("time_left = %d\n", time_left);
 
         if (time_left - time_unit < 0) {
             if (time_fly <= time_left) {
@@ -18,11 +21,11 @@ int distance(int speed, int time_fly, int time_rest) {
             } else {
                 distance += speed*time_left;
             }
-            printf("distance = %d\n", distance);
+            // printf("distance = %d\n", distance);
             break;
         } else {
             distance += speed*time_fly;
-            printf("distance = %d\n", distance);
+            // printf("distance = %d\n", distance);
         }
 
         time_left -= time_unit;
@@ -32,8 +35,24 @@ int distance(int speed, int time_fly, int time_rest) {
 
 
 int part1(char *data) {
-    printf("%s\n", data);
-    return 0;
+    int distances[REINDEERS];
+    int i = 0;
+    char *line = strtok(data, "\n");
+    while (line) {
+        int speed = -1, fly = -1, rest = -1;
+        char tmp[32] = {0};
+        sscanf(line, "%s can fly %d km/s for %d seconds, but then must rest for %d seconds.", tmp, &speed, &fly, &rest);
+        distances[i++] = distance(speed, fly, rest);
+	    line = strtok(NULL, "\n");
+    }
+
+    int max = distances[0];
+    for (size_t i = 0; i < REINDEERS; i++) {
+        if (distances[i] > max) {
+            max = distances[i];
+        }
+    }
+    return max;
 }
 
 int part2(char *data) {
@@ -43,8 +62,12 @@ int part2(char *data) {
 int main(int argc, char** argv) {
     (void)argc;
     char *input_file = argv[1];
-    char *data = read_to_str(input_file);
+    size_t count = 0;
+    char *data = read_to_str_size(input_file, &count);
 
+    // part1 uses strtok which destroys the original string
+    char *part1_data = malloc(count);
+    strcpy(part1_data, data);
     printf("Part 1: %d\n", part1(data));
     printf("Part 2: %d\n", part2(data));
     return 0;
